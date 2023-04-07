e934b00e7e4a1943b061277289e8c8e1f7bce296
Ñîçø <nightOwlNico@gmail.com>
2023-04-07 12:02:20 -0600

    configured sendgrid api settings

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+app.post('/sendgrid-webhook', async (req, res) => {
+  try {
+    const { to, from, subject, text } = req.body;
+    const msg = {
+      to: 'nightOwlNico@gmail.com',
+      from: from.email,
+      subject: `Forwarded: ${subject}`,
+      text: `Original sender: ${from.email}\n\n${text}`,
+    };
+
+    await sgMail.send(msg);
+    res.status(200).send('Email forwarded successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error forwarding email');
+  }
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+app.listen(3000, () => console.log('Server running on port 3000'));
