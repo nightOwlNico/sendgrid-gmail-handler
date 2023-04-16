f3c2f817e0d2ce662b5af32a434ded51cd59a06f
Ñîçø <nightOwlNico@gmail.com>
2023-04-16 10:39:44 -0600

    raw attempt

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
 const express = require('express');
 const multer = require('multer');
 const sgMail = require('@sendgrid/mail');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 app.post('/sendgrid-webhook', upload.any(), async (req, res) => {
   // console.log('req.body', req.body);
 
-  try {
-    const {
-      from,
-      subject,
-      text,
-      html,
-      'attachment-info': attachmentInfo,
-    } = req.body;
-    if (!from) {
-      return res.status(400).send('Missing required field: from');
-    }
+  if (!req.body.from) {
+    return res.status(400).send('Missing required field: from');
+  }
 
-    // Parse the attachment-info JSON string
-    const parsedAttachmentInfo = JSON.parse(attachmentInfo);
+  const emailData = req.body;
+  const fromEmail = emailData.from;
+  const subject = emailData.subject;
+  const textContent = emailData.text;
+  const htmlContent = emailData.html;
 
-    // Create an array of attachments with the required format
-    const attachments = req.files.map((file) => ({
+  // Process attachments
+  const attachments = req.files.map((file) => {
+    return {
       content: file.buffer.toString('base64'),
       filename: file.originalname,
       type: file.mimetype,
       disposition: 'attachment',
-      contentId: parsedAttachmentInfo[file.fieldname]['content-id'],
-    }));
-
-    const msg = {
-      to: process.env.TO_EMAIL,
-      from: process.env.FROM_EMAIL,
-      replyTo: from,
-      subject: subject,
-      text: text,
-      html: html,
-      attachments: attachments.length > 0 ? attachments : undefined,
     };
+  });
+
+  // Create and send email using SendGrid
+  const msg = {
+    to: process.env.TO_EMAIL,
+    from: process.env.FROM_EMAIL,
+    replyTo: fromEmail,
+    subject: subject,
+    text: textContent,
+    html: htmlContent,
+    attachments: attachments,
+  };
+
     // console.log('Sending message:', msg);
+    
     await sgMail.send(msg);
     res.status(200).send('Email forwarded successfully');
   } catch (error) {
     console.error('Error:', error);
-    if (error.response) {
+    if (error.response && error.response.body) {
       console.error('Error response body:', error.response.body);
     }
     res.status(500).send('Error forwarding email');
