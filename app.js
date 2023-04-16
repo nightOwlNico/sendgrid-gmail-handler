39d387d761175c3dd35cc9f7215b14ad89fa3eb0
Ñîçø <nightOwlNico@gmail.com>
2023-04-16 11:22:51 -0600

    only logging

--- app.js
+++ app.js
@@ -11,41 +11,42 @@ const upload = multer({ storage: storage });
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.post('/sendgrid-webhook', upload.any(), async (req, res) => {
-  // console.log('req.body', req.body);
+  console.log('req', req);
+  console.log('res', res);
 
-  if (!req.body.from) {
-    return res.status(400).send('Missing required field: from');
-  }
+  // if (!req.body.from) {
+  //   return res.status(400).send('Missing required field: from');
+  // }
 
-  const emailData = req.body;
-  const fromEmail = emailData.from;
-  const subject = emailData.subject;
-  const textContent = emailData.text;
-  const htmlContent = emailData.html;
+  // const emailData = req.body;
+  // const fromEmail = emailData.from;
+  // const subject = emailData.subject;
+  // const textContent = emailData.text;
+  // const htmlContent = emailData.html;
 
-  console.log('Text content:', textContent);
-  console.log('HTML content:', htmlContent);
+  // console.log('Text content:', textContent);
+  // console.log('HTML content:', htmlContent);
 
-  // Process attachments
-  const attachments = req.files.map((file) => {
-    return {
-      content: file.buffer.toString('base64'),
-      filename: file.originalname,
-      type: file.mimetype,
-      disposition: 'attachment',
-    };
-  });
+  // // Process attachments
+  // const attachments = req.files.map((file) => {
+  //   return {
+  //     content: file.buffer.toString('base64'),
+  //     filename: file.originalname,
+  //     type: file.mimetype,
+  //     disposition: 'attachment',
+  //   };
+  // });
 
-  // Create and send email using SendGrid
-  const msg = {
-    to: process.env.TO_EMAIL,
-    from: process.env.FROM_EMAIL,
-    replyTo: fromEmail,
-    subject: subject,
-    text: textContent,
-    html: htmlContent,
-    attachments: attachments,
-  };
+  // // Create and send email using SendGrid
+  // const msg = {
+  //   to: process.env.TO_EMAIL,
+  //   from: process.env.FROM_EMAIL,
+  //   replyTo: fromEmail,
+  //   subject: subject,
+  //   text: textContent,
+  //   html: htmlContent,
+  //   attachments: attachments,
+  // };
 
   // console.log('Sending message:', msg);
 
